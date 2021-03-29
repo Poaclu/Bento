69bfff1e2b7a57d4505a9ecd1e21836a7b606035
Luc Irwin <25772718+Poaclu@users.noreply.github.com>
2021-03-29 09:49:09 +0000

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,23 +3,24 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' Luc!';
+const name = ' Luc';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
+const gree1 = 'Go to Sleep  ';
+const gree2 = 'Good morning,  ';
 const gree3 = 'Good afternoon  ';
 const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+const points = 'Good evening,  ';
+const oh = '!';
+const what = '?';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById('greetings').innerText = gree1 + name + points;
 } else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
+  document.getElementById('greetings').innerText = gree2 + name + oh;
 } else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
+  document.getElementById('greetings').innerText = gree3 + name + oh;
 } else {
-  document.getElementById('greetings').innerText = gree4 + name;
+  document.getElementById('greetings').innerText = gree4 + name oh;
 }
