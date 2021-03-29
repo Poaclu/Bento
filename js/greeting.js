4ba6bf0956688e21b105edaa0737885ffbc57b25
Luc Irwin <25772718+Poaclu@users.noreply.github.com>
2021-03-29 13:57:37 +0000

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -1,5 +1,5 @@
 // Get the hour
-const today =  new Date;
+const today =  new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' });
 const hour = today.getHours();
 
 // Here you can change your name
